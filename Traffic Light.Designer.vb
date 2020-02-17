36a1417fee1fb1a2fc59b90b114af358ad0fcd0e
NoahSK23 <noahsklinger@gmail.com>
2020-02-17 17:47:19 -0500

    Add project files.

---
+++ Traffic Light.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTrafficLight
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtGreenLight = New System.Windows.Forms.TextBox()
+        Me.txtYellowLight = New System.Windows.Forms.TextBox()
+        Me.txtRedLight = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtGreenLight
+        '
+        Me.txtGreenLight.BackColor = System.Drawing.Color.Gray
+        Me.txtGreenLight.Location = New System.Drawing.Point(147, 375)
+        Me.txtGreenLight.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtGreenLight.Multiline = True
+        Me.txtGreenLight.Name = "txtGreenLight"
+        Me.txtGreenLight.Size = New System.Drawing.Size(125, 125)
+        Me.txtGreenLight.TabIndex = 0
+        '
+        'txtYellowLight
+        '
+        Me.txtYellowLight.BackColor = System.Drawing.Color.Gray
+        Me.txtYellowLight.Location = New System.Drawing.Point(147, 199)
+        Me.txtYellowLight.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtYellowLight.Multiline = True
+        Me.txtYellowLight.Name = "txtYellowLight"
+        Me.txtYellowLight.Size = New System.Drawing.Size(125, 125)
+        Me.txtYellowLight.TabIndex = 1
+        '
+        'txtRedLight
+        '
+        Me.txtRedLight.BackColor = System.Drawing.Color.Gray
+        Me.txtRedLight.Location = New System.Drawing.Point(147, 23)
+        Me.txtRedLight.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtRedLight.Multiline = True
+        Me.txtRedLight.Name = "txtRedLight"
+        Me.txtRedLight.Size = New System.Drawing.Size(125, 125)
+        Me.txtRedLight.TabIndex = 2
+        '
+        'frmTrafficLight
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 554)
+        Me.Controls.Add(Me.txtRedLight)
+        Me.Controls.Add(Me.txtYellowLight)
+        Me.Controls.Add(Me.txtGreenLight)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmTrafficLight"
+        Me.Text = "Traffic Light"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtGreenLight As TextBox
+    Friend WithEvents txtYellowLight As TextBox
+    Friend WithEvents txtRedLight As TextBox
+End Class
---
+++ Traffic Light.vb
@@ -0,0 +1,28 @@
+﻿'Date: Feb. 12 2020
+'Instructor: Ralph Hunsberger
+'Traffic Light
+'Description: This project changes the color of each text box by clicking the tab key.
+
+Option Explicit On
+Public Class frmTrafficLight
+    Private Sub txtGreenLight_GotFocus(sender As Object, e As EventArgs) Handles txtGreenLight.GotFocus
+        txtGreenLight.BackColor = Color.Green
+    End Sub
+    Private Sub txtGreenLight_Leave(sender As Object, e As EventArgs) Handles txtGreenLight.Leave
+        txtGreenLight.BackColor = Color.Gray
+    End Sub
+    Private Sub txtYellowLight_GotFocus(sender As Object, e As EventArgs) Handles txtYellowLight.GotFocus
+        txtYellowLight.BackColor = Color.Yellow
+    End Sub
+
+    Private Sub txtYellowLight_Leave(sender As Object, e As EventArgs) Handles txtYellowLight.Leave
+        txtYellowLight.BackColor = Color.Gray
+    End Sub
+    Private Sub txtRedLight_GotFocus(sender As Object, e As EventArgs) Handles txtRedLight.GotFocus
+        txtRedLight.BackColor = Color.Red
+    End Sub
+
+    Private Sub txtRedLight_Leave(sender As Object, e As EventArgs) Handles txtRedLight.Leave
+        txtRedLight.BackColor = Color.Gray
+    End Sub
+End Class
